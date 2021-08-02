e68d8df713b2675e125db98419c6a0410ecfa50c
Douglas <douglaspereira04@hotmail.com>
2021-08-02 08:39:25 -0300

    Adds regex exceptions

---
+++ src/main/java/exception/regex/BracketMismatchException.java
@@ -0,0 +1,18 @@
+package exception.regex;
+
+public class BracketMismatchException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2573039199136807300L;
+
+	public BracketMismatchException(String errMessage) {
+		super(errMessage);
+	}
+	
+	public BracketMismatchException() {
+		super("Bracket Mismatch");
+	}
+
+}
---
+++ src/main/java/exception/regex/InvalidInputException.java
@@ -0,0 +1,14 @@
+package exception.regex;
+
+public class InvalidInputException extends Exception {
+	
+	private static final long serialVersionUID = 2573039199136807300L;
+
+	public InvalidInputException(String errMessage) {
+		super(errMessage);
+	}
+	
+	public InvalidInputException() {
+		super("Invalid Input");
+	}
+}
